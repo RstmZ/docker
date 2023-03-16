33f53314c3ed33e77fb06458eeacb2b3ad7ef939
Rstm.ramaz <rstm.ramaz@gmail.com>
2023-03-16 15:49:12 +0200

    add pull image

--- src/app.ts
+++ src/app.ts
@@ -6,20 +6,36 @@ const app: Express = express();
 
 app.use(express.json());
 
+async function isExist(name: string) {
+    try {
+        return await system(`docker inspect ${name}`);
+    } catch (error) {
+        if (error === `Error: No such object: ${name}`) {
+            console.log('error');
+            
+            await system(`docker pull ${name}`);
+            return await system(`docker inspect ${name}`);
+        }
+        throw new Error(error as string);
+    }
+}
+
 app.post('/', async (req: Request, res: Response) => {
     try {
-        const body = req.body;
-        const output = await system(`docker inspect ${body.image}`);
+        const name = req.body.name;
+
+        const output = await isExist(name);
+ 
         if (output.length == 0) {
             res.status(404).send('error')
         }
-        
+
         const result = JSON.parse(output)[0]
-  
+
         const answer = pick(result, 'Created', 'Parent', 'Author', 'ContainerConfig.Hostname', 'ContainerConfig.Cmd', 'RootFS')
         res.json(answer);
     } catch (error) {
-        res.status(404).send({error})
+        res.status(404).send({ error })
     }
 });
 
